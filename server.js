93095ed72ef06750c3f206596825e384377c346b
Raj kumar mishra <133225272+Raj9134@users.noreply.github.com>
2026-05-03 04:19:21 +0530

    fix railway port issue

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB(); 
+connectDB();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("server is running on port " + PORT);
 });
